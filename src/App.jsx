486909211bef9b59a3d6c294b5eda317f4884c71
unknown <vedatrayani.kosuri@gmail.com>
2026-03-02 10:48:10 +0530

    Added my name below title

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,24 @@
 import { useEffect, useState } from "react";
+
 function App() {
- const [message, setMessage] = useState("");
- useEffect(() => {
- fetch("https://sb-backend-mrnc.onrender.com/api/hello")
- .then(res => res.text())
- .then(data => setMessage(data))
- .catch(err => console.error(err));
- }, []);
- return (
- <div style={{ textAlign: "center", marginTop: "50px" }}>
- <h1>Spring Boot + React App</h1>
- <h2>{message}</h2>
- </div>
- );
+  const [message, setMessage] = useState("");
+
+  useEffect(() => {
+    fetch("https://sb-backend-mrnc.onrender.com/api/hello")
+      .then(res => res.text())
+      .then(data => setMessage(data))
+      .catch(err => console.error(err));
+  }, []);
+
+  return (
+    <div style={{ textAlign: "center", marginTop: "50px" }}>
+      <h1>Spring Boot + React App</h1>
+      <h2>{message}</h2>
+      <p style={{ fontSize: "18px", color: "gray" }}>
+        Vedaa!!
+      </p>
+    </div>
+  );
 }
+
 export default App;
